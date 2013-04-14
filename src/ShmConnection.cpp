06fbb8b548db37be7ea623dbd53420dfdae202bd
Adrien Anselme <adrien@springbeats.com>
2013-04-14 23:07:32 +0200

    first step to shared mem channel : ShmConnection class

---
+++ src/ShmConnection.cpp
@@ -0,0 +1,9 @@
+//
+//  ShmConnection.cpp
+//  libpbrpcpp
+//
+//  Created by Adrien Anselme on 4/13/13.
+//
+//
+
+#include "ShmConnection.h"
---
+++ src/ShmConnection.h
@@ -0,0 +1,14 @@
+//
+//  ShmConnection.h
+//  libpbrpcpp
+//
+//  Created by Adrien Anselme on 4/13/13.
+//
+//
+
+#ifndef __libpbrpcpp__ShmConnection__
+#define __libpbrpcpp__ShmConnection__
+
+#include <iostream>
+
+#endif /* defined(__libpbrpcpp__ShmConnection__) */
